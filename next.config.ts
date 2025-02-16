5e9698b43ebe375de7d71640f7a126e510f4ed91
Filipee <thefilipee@gmail.com>
2025-02-16 10:55:12 +0100

    feat: Github pages configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/volne-recepty',
 };
 
 export default nextConfig;
